2f30d3c0f215bbc2cccb7a596910ba25e6cc9e3e
Crowdbotics <team@crowdbotics.com>
2021-08-23 07:57:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/orlando_for_sale_si_30036/MainActivity.java
@@ -0,0 +1,15 @@
+package com.orlando_for_sale_si_30036;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "orlando_for_sale_si_30036";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "orlando_for_sale_si_30036Identifier",
+  url: "https://orlando_for_sale_si_30036Identifier.botics.co",
+  api: "https://orlando_for_sale_si_30036Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
